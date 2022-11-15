35e67d1ea204a063b9e383f691c597f3b1e04268
Daniel Z. Juca <daniel3959@hotmail.com>
2022-11-15 12:05:52 +0000

    global data function

--- energyBalance/energyBalanceMain.m
+++ energyBalance/energyBalanceMain.m
@@ -1,31 +1,29 @@
-% ----------------- SIMULACI�N - REACTOR LECHO FLUIDIZADO -----------------
-%                      REFORMADO SECO DE METANO (DRM)
-% Proyecto BioRefinEr - Laboratorio de Reactores Qu�micos - CREG - I3A 
-%     Daniel Z. Juca  - Estudiante de Doctorado
-%     Miguel Men�ndez - Director
-%     Jaime Soler     - Co-director
-% ------------------------ | INICIO RLF.m | -------------------------------
+% -------------------------------------------------------------------------
+% ------------------------ FLUIDIZED BED REACTOR --------------------------
+%                      DRY REFORMING OF METHANE (DRM)
+%                         ENERGY BALANCE SOLUTION
+%     Author: Daniel Z. Juca
+% ------------------------ | initiation | ---------------------------------
   close all
-  clear;
+  clear
   clc
 % ---------- Constantes de tipo global ------------------------------------
   global ncall n Num_esp
            constantesFBR
   NoN = (1:n*Num_esp);
-% ---------- Inicio de constantes iniciales -------------------------------
+% ---------- initial condition --------------------------------------------
     u0 = inicial;
-% ---------- tiempo de simulaci�n (s) -------------------------------------
+% ---------- time simulation (s) ------------------------------------------
     t0 = 0.0; 
     tf = 3600*1000; 
   tout = linspace(t0,tf,100)';
-% ---------- Integraci�n Ecuaciones Diferenciales Ordianarias (ODEs) ------ 
-  reltol = 1.0e-6; abstol = 1.0e-6;  
- options = odeset('RelTol',reltol,'AbsTol',abstol,'NonNegative',NoN);
 % ---------- Implicit (sparse stiff) integration --------------------------
+   reltol = 1.0e-6; abstol = 1.0e-6;  
+  options = odeset('RelTol',reltol,'AbsTol',abstol,'NonNegative',NoN);
         S = JPatternDRM;
   options = odeset(options,'JPattern',S); 
     [t,u] = ode15s(@pdeDRM,tout,u0,options);  
 % ---------- Salidas ------------------------------------------------------
-  [CiBW,CiE] = SkinFcnMac(t,u);
+  [T_b,T_e,T] = SkinFcnMac(t,u);
    fprintf('\n ncall = %4d\n',ncall);
-% ---------------------------| FIN RLF.m |---------------------------------
+% ---------------------------| End Program |-------------------------------
---
+++ energyBalance/globalData.m
@@ -0,0 +1,5 @@
+function g = globalData()
+
+    
+
+end
