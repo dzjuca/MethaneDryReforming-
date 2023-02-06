7de642615664a0e9c40f8a2f7d932b3195a852b4
Daniel Z. Juca <daniel3959@hotmail.com>
2023-02-06 16:25:57 +0000

    energy balance update

--- globalBalance.m
+++ globalBalance.m
@@ -14,13 +14,13 @@
          u0 = initialConditions(Global);
 % ---------- time simulation (s) ------------------------------------------
     t0 = 0.0; 
-    tf = 30; 
+    tf = 360; 
   tout = linspace(t0,tf,100)';
 % ---------- Implicit (sparse stiff) integration --------------------------
    reltol = 1.0e-6; abstol = 1.0e-6;  
   options = odeset('RelTol',reltol,'AbsTol',abstol,'NonNegative',NoN);
-        S = JPatternFcn(Global);
-  options = odeset(options,'JPattern',S); 
+%         S = JPatternFcn(Global);
+%   options = odeset(options,'JPattern',S); 
  pdeModel = @(t,u)pdeFcn(t,u,Global);
     [t,u] = ode15s(pdeModel,tout,u0,options);  
 % ---------------------------| End Program |-------------------------------
